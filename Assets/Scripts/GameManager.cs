169c7fbbcade4bcd8549fb3b3b1b14e4b668f452
Vivinator <vivienne2.nip@live.uwe.ac.uk>
2018-02-16 01:39:10 +0000

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public float levelStartDelay = 2f;
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private Text levelText;
+    private GameObject levelImage;
+    private int level = 1;
+    private bool doingSetup;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        //InitGame();
+	}
+
+    //This is called each time a scene is loaded.
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        //Add one to our level number.
+        level++;
+        //Call InitGame to initialize our level.
+        InitGame();
+    }
+
+    void OnEnable()
+    {
+        //Tell our ‘OnLevelFinishedLoading’ function to start listening for a scene change event as soon as this script is enabled.
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnDisable()
+    {
+        //Tell our ‘OnLevelFinishedLoading’ function to stop listening for a scene change event as soon as this script is disabled.
+        //Remember to always have an unsubscription for every delegate you subscribe to!
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    void InitGame()
+    {
+        doingSetup = true;
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Level " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
+        boardScript.SetupScene(level);
+    }
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    	
+	}
+}
